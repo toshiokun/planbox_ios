93ef488dd61e77b999c8ee70216182ce68d03c92
Toshihiro Taniguchi <10401040.taniyama@gmail.com>
2016-01-21 15:08:59 +0900

    Initial Commit

---
+++ PlanBox/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PlanBox
+//
+//  Created by Taniguchi Toshihiro on 2016/01/21.
+//  Copyright © 2016年 mycompany. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PlanBox/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PlanBox
+//
+//  Created by Taniguchi Toshihiro on 2016/01/21.
+//  Copyright © 2016年 mycompany. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
