74f5d95214d63c7c541472f222ac3af316d6b96d
Toshihiro Taniguchi <10401040.taniyama@gmail.com>
2016-02-18 18:07:10 +0900

    syokai

--- PlanBox/FirstViewController.swift
+++ PlanBox/FirstViewController.swift
@@ -8,11 +8,45 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    
+    @IBOutlet weak var maleTableView: UITableView!
+    
+    let imgArray: NSArray = ["man-7","man-7","man-7","man-7","man-7","man-7","man-7","man-7"]
+    
+    let labelArray: NSArray = ["2013/8/23/16:04","2013/8/23/16:15","2013/8/23/16:47","2013/8/23/17:10","2013/8/23/1715:","2013/8/23/17:21","2013/8/23/17:33","2013/8/23/17:41"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func tableView(table: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return imgArray.count
+    }
+    
+    //各セルの要素を設定する
+    func tableView(table: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        // tableCell の ID で UITableViewCell のインスタンスを生成
+        let cell = table.dequeueReusableCellWithIdentifier("tableCell", forIndexPath: indexPath)
+        
+        let img = UIImage(named:"\(imgArray[indexPath.row])")
+        // Tag番号 1 で UIImageView インスタンスの生成
+        let imageView = table.viewWithTag(1) as! UIImageView
+        imageView.image = img
+        
+        // Tag番号 ２ で UILabel インスタンスの生成
+        let label1 = table.viewWithTag(2) as! UILabel
+        label1.text = "No.\(indexPath.row + 1)"
+        
+        // Tag番号 ３ で UILabel インスタンスの生成
+        let label2 = table.viewWithTag(3) as! UILabel
+        label2.text = "\(labelArray[indexPath.row])"
+        
+        
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
