0c06f40750d498712be351628e5cd38cdac4d3a3
Toshihiro Taniguchi <10401040.taniyama@gmail.com>
2016-02-22 23:06:45 +0800

    Swift yoku wakaranai

--- PlanBox/ViewController.swift
+++ PlanBox/ViewController.swift
@@ -11,8 +11,13 @@ import UIKit
 class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
     @IBOutlet weak var agePicker: UIPickerView!
+    @IBOutlet weak var characterPicker: UIPickerView!
+    @IBOutlet weak var loveNumberPicker: UIPickerView!
+    
     
     let compos = [[Int](0...99)]
+    let compos1 = [["おとなしい","賑やか","性格がいい"]]
+    let compos2 = [[Int](0...50)]
     
     
     func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
@@ -20,13 +25,33 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     }
     
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        let compo = compos[component]
-        return compo.count
+        var rowsNumber = 0
+        if(pickerView.tag == 0) {
+            let compo = compos[component]
+            rowsNumber = compo.count
+        } else if (pickerView.tag == 1) {
+            let compo = compos1[component]
+            rowsNumber = compo.count
+        } else if (pickerView.tag == 1) {
+            let compo = compos2[component]
+            rowsNumber = compo.count
+        }
+        return rowsNumber
     }
     
     func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        let item = compos[component][row]
-        return String(item)
+        var item :String?
+        if(pickerView.tag == 0) {
+            let number = compos[component][row]
+            item = String(number)
+        } else if (pickerView.tag == 1) {
+            let character = compos1[component][row]
+            item = character
+        } else if (pickerView.tag == 1) {
+            let loveNum = compos2[component]
+            item = String(loveNum)
+        }
+        return item
     }
     
     override func viewDidLoad() {
@@ -34,6 +59,14 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         
         agePicker.delegate = self
         agePicker.dataSource = self
+        characterPicker.delegate = self
+        characterPicker.dataSource = self
+        loveNumberPicker.delegate = self
+        loveNumberPicker.dataSource = self
+        
+        agePicker.tag = 0
+        characterPicker.tag = 1
+        loveNumberPicker.tag = 2
 
         // Do any additional setup after loading the view.
     }
